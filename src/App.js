3af0761191cbe4bbe8145dd6eb255960a5ac1721
DriscollRichie <driscollrichie@gmail.com>
2018-05-11 10:12:12 -0600

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard/Dashboard'
+import Wizard from './components/Wizard/Wizard'
+import Header from './components/Header/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <Dashboard/>
+        <Wizard/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/House/House.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function House() {
+  return(
+    <div>
+      <h1>I am House</h1>
+    </div>
+  )
+}
